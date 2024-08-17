e4e542678d13cbd55f8aaa3664fcff40e0f49fa4
Jeyson Jaramillo <jjaramillo@viamatica.com>
2024-08-17 08:49:12 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MiprimercomponenteComponent } from './ejemplos/miprimercomponente/miprimercomponente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiprimercomponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
