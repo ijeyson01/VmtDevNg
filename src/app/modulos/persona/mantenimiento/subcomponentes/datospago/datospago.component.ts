81626f87437b3e0971fb2ec5c5c349e5faeb82fd
Jeyson Jaramillo <jjaramillo@viamatica.com>
2024-08-27 17:59:35 -0500

    Ejercicio de modulos y viewchild

--- src/app/modulos/persona/mantenimiento/subcomponentes/datospago/datospago.component.ts
+++ src/app/modulos/persona/mantenimiento/subcomponentes/datospago/datospago.component.ts
@@ -10,4 +10,16 @@ export class DatospagoComponent {
   guardarDatosPago() {
     
   }
+
+  /**
+   * 1. Construir un módulo para el ingreso de productos a un inventario
+   * 1.1. El módulo manejará los siguientes procesos <-> componentes
+   * - Datos del producto: Nombre, tipo (Electrodoméstico, técnología, varios), descripción y datos adicionales 
+   * - Datos de precio (Precio unitario, aplica IVA, aplica descuento, descuento y PVP)
+   * - Datos de proveedor (Nombre proveedor, descripcion proveedor, precio proveedor)
+   * 
+   * 1.2 El módulo debe validar que el precio unitario y el precio del proveedor sean el mismo
+   * 1.3 El descuento no puede ser 0% ni mayor al 8%
+   * 
+   */
 }
